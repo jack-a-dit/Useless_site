859dd36a9eb09ce548c6572664b995e75c1a6e58
Adrien Mallet <76951388+jack-a-dit@users.noreply.github.com>
2026-04-14 00:57:44 +0200

    feat: ajout d'input + css
    ajout:
     - d'un input pour deviner le pays
     - d'un compteur du nombre d'erreur
    Modification:
     - réagencement du component interface

--- ui/src/app/global_componnents/Interface/interface.ts
+++ ui/src/app/global_componnents/Interface/interface.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
+import { FormsModule } from '@angular/forms';
 import { CountryModel } from '../../models/countryModel';
 
 @Component({
   selector: 'app-interface',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './interface.html',
   styleUrls: ['./interface.css']
 })
@@ -16,4 +18,12 @@ export class Interface {
       population: 0,
       map: ''
     }
+    guess: string = '';
+    attemptsLeft: number = 3;
+    isWrong: boolean = false;
+
+    CheckCountry() {
+        // Implementation for checking the country
+    }
+
 }
